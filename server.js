d62047179208376b25a3a5745e56001e234c2433
Joy Sinha <39700727+joy854@users.noreply.github.com>
2020-04-26 23:21:04 +0530

    db

--- server.js
+++ server.js
@@ -5,14 +5,8 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json'
-  );
   next();
 });
 const knex = require('knex');
